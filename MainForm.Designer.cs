4d2be3f8b47e369316594fadb986684027357f7d
unknown <shamsatia96@gmail.com>
2024-07-17 15:08:13 +0300

    Add All Codes

---
+++ MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace First_Project
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            button1 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.MenuHighlight;
+            button1.ImageKey = "(none)";
+            button1.Location = new Point(227, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 43);
+            button1.TabIndex = 2;
+            button1.Text = "Add Member";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(835, 68);
+            panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.MenuHighlight;
+            label1.Location = new Point(795, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 36);
+            label1.TabIndex = 4;
+            label1.Text = "x";
+            label1.Click += label1_Click;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.System;
+            button3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.MenuHighlight;
+            button3.ImageKey = "(none)";
+            button3.Location = new Point(644, 11);
+            button3.Name = "button3";
+            button3.Size = new Size(133, 43);
+            button3.TabIndex = 5;
+            button3.Text = "Payment";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.System;
+            button4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.MenuHighlight;
+            button4.ImageKey = "(none)";
+            button4.Location = new Point(505, 11);
+            button4.Name = "button4";
+            button4.Size = new Size(133, 43);
+            button4.TabIndex = 6;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(61, 68);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.System;
+            button2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.MenuHighlight;
+            button2.ImageKey = "(none)";
+            button2.Location = new Point(366, 11);
+            button2.Name = "button2";
+            button2.Size = new Size(133, 43);
+            button2.TabIndex = 4;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(837, 469);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainForm";
+            Text = "MainForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+    }
+}
